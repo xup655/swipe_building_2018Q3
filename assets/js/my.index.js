7b57e134c8b03ccafb85620298e4a58854958d41
xup655 <xup655@hotmail.com>
2018-09-21 18:35:17 +0800

    commit

--- assets/js/my.index.js
+++ assets/js/my.index.js
@@ -12,54 +12,59 @@ $(document).ready(function(){
     //         prevEl: '.swiper-button-prev',
     //     },
     //     observer: true,
-    //     observeParents: true
+    //     observeParents: true,
+    //     pagination: {
+    //         el: '.swiper-pagination-v',
+    //         clickable: true,
+    //     },
+    //     // watchSlidesProgress: true,
+    //     // watchSlidesVisibility: true,
     // });
 
     var swiper2 = new Swiper('.swiper-container[stype="t1"]', {
         direction:"horizontal",
         speed: 1000,
-        slidesPerView: 3,
+        slidesPerView: 1,
         spaceBetween: 30,
         slidesPerGroup: 3,
         navigation: {
             nextEl: '.swiper-button-next[stype="t1"]',
             prevEl: '.swiper-button-prev[stype="t1"]',
         },
-        // pagination: {
-        //     el: '.swiper-pagination-v',
-        //     clickable: true,
-        // },
         observer: true,
         observeParents: true
     });
 
+    // $('.team_menu_list li a').click(function(){
+    //     var inx=$(this).attr('inx');
+    //     console.log(swipers);
+    //     alert(">>" + swipers.activeIndex); // always 0
+    //     swipers.slideTo(inx, 300, true);
+    // });
+
     $('.overlay2 .team_menu_list li a').click(function(){
-        var inx=$(this).attr('inx');
-        swiper2.slideTo(inx, 300, function(){
-        });
+        var inx = $(this).attr('inx');
+        swiper2.slideTo(2 , 0);
+        console.log(swiper2)
     });
 
     var swiper3 = new Swiper('.swiper-container[stype="t2"]', {
         direction:"horizontal",
         speed: 1000,
-        slidesPerView: 3,
+        slidesPerView: 1,
         spaceBetween: 30,
         slidesPerGroup: 3,
         navigation: {
             nextEl: '.swiper-button-next[stype="t2"]',
             prevEl: '.swiper-button-prev[stype="t2"]',
         },
-        // pagination: {
-        //     el: '.swiper-pagination-v',
-        //     clickable: true,
-        // },
         observer: true,
         observeParents: true
     });
 
     $('.overlay1 .team_menu_list li a').click(function(){
-        var inx=$(this).attr('inx');
-        swiper3.slideTo(inx, 300, function(){
-        });
-    });
+        var inx = $(this).attr('inx');
+        swiper3.slideTo(2 , 0);
+        console.log(swiper3)
+    }); 
 });
