29f108e137c3607a05140722f80e9021cd30fc56
Stephanie <stephanie@wabow.com>
2018-10-02 16:08:33 +0800

    PC safari test4

--- assets/js/index.js
+++ assets/js/index.js
@@ -303,7 +303,11 @@ $(document).ready(function(){
         if( $('#project').hasClass('swiper-slide-active')) {
             // setTimeout(function() {
                 if( !$('.fadein-card').hasClass('scrollin') ) {
-                    $('.fadein-card').addClass('scrollin')
+                    // $('.fadein-card').addClass('scrollin')
+                    // $('.swiper-pagination').first().find('.swiper-pagination-bullet').eq(2).click();
+                    setTimeout(function() {
+                        $('.fadein-card').addClass('scrollin')
+                    }, 100);
                 }
             // }, 800);
         }
